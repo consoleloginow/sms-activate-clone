b48c3957c95986f58bf1682298e02dc8a47d2c7b
consoleloginow <186641881+consoleloginow@users.noreply.github.com>
2025-04-16 23:43:13 +0300

    shadcn-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', 'shadcn-nuxt'],
   css: ['~/assets/css/main.css'],
 })
